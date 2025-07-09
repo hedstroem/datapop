cc23695413938fb1eb84e434d5e5fcc76bdd8e21
Lukas <id19lhm@cs.umu.se>
2025-07-09 18:44:32 +0200

    Favicon + Small fixes

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,12 +1,5 @@
 
 export default new Map([
-["src/content/blog/gradient-descent.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgradient-descent.mdx&astroContentModuleFlag=true")],
-["src/content/blog/binary-search-variations.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbinary-search-variations.mdx&astroContentModuleFlag=true")],
-["src/content/blog/neural-network-from-scratch.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fneural-network-from-scratch.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/how-to-write-posts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-to-write-posts.mdx&astroContentModuleFlag=true")],
-["src/content/blog/how-to-write-posts copy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-to-write-posts+copy.mdx&astroContentModuleFlag=true")],
-["src/content/blog/how-to-write-posts copy 2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-to-write-posts+copy+2.mdx&astroContentModuleFlag=true")],
-["src/content/blog/how-to-write-posts copy 3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-to-write-posts+copy+3.mdx&astroContentModuleFlag=true")],
-["src/content/blog/how-to-write-posts copy 4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-to-write-posts+copy+4.mdx&astroContentModuleFlag=true")],
-["src/content/blog/how-to-write-posts copy 5.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-to-write-posts+copy+5.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/gradient-descent.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgradient-descent.mdx&astroContentModuleFlag=true")]]);
 		
