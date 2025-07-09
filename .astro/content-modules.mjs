51849f282743a6a255cd2eb59068aae7e72da1e0
Lukas <id19lhm@cs.umu.se>
2025-07-09 18:54:43 +0200

    instant.page testing

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,5 @@
 
 export default new Map([
-["src/content/blog/how-to-write-posts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-to-write-posts.mdx&astroContentModuleFlag=true")],
-["src/content/blog/gradient-descent.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgradient-descent.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/gradient-descent.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgradient-descent.mdx&astroContentModuleFlag=true")],
+["src/content/blog/how-to-write-posts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-to-write-posts.mdx&astroContentModuleFlag=true")]]);
 		
