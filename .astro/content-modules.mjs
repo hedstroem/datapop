0a999a85ce7a3ec0a97bfe6c444124d960cce41c
hedstroem <john.s.hedstrom@gmail.com>
2025-07-15 20:08:06 +0200

    finalize page, make it only focus on incremental median estimation

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,4 @@
 
 export default new Map([
-["src/content/blog/how-to-write-posts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-to-write-posts.mdx&astroContentModuleFlag=true")],
-["src/content/blog/gradient-descent.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgradient-descent.mdx&astroContentModuleFlag=true")],
-["src/content/blog/regularization-techniques-deep-dive.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fregularization-techniques-deep-dive.mdx&astroContentModuleFlag=true")],
-["src/content/blog/incremental-quantile-estimation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-quantile-estimation.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/incremental-median-estimation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-median-estimation.mdx&astroContentModuleFlag=true")]]);
 		
