3c18d04e3d1393d9c9aa0031208ffa1b4f956d64
Lukas <id19lhm@cs.umu.se>
2025-07-17 19:14:02 +0200

    Image Carousel alternative added

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
-["src/content/blog/incremental-median-estimation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-median-estimation.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/incremental-median-estimation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-median-estimation.mdx&astroContentModuleFlag=true")],
+["src/content/blog/image-gallery-demo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fimage-gallery-demo.mdx&astroContentModuleFlag=true")]]);
 		
