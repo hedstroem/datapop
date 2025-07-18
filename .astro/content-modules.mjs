2e83d3289f8acf731e31f43868209ded2c031054
hedstroem <john.s.hedstrom@gmail.com>
2025-07-18 11:22:45 +0200

    update topics, add reading list

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,6 @@
 
 export default new Map([
-["src/content/blog/image-gallery-demo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fimage-gallery-demo.mdx&astroContentModuleFlag=true")],
-["src/content/blog/incremental-median-estimation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-median-estimation.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/incremental-median-estimation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-median-estimation.mdx&astroContentModuleFlag=true")],
+["src/content/blog/reading-list.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Freading-list.mdx&astroContentModuleFlag=true")],
+["src/content/blog/demo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdemo.mdx&astroContentModuleFlag=true")]]);
 		
