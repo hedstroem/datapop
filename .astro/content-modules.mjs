035dfe4a7a7e69dc5b1617214eaaaed9aaaa6be1
hedstroem <john.s.hedstrom@gmail.com>
2025-07-21 14:40:32 +0200

    tweaked the end of the incremental quantile post

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
-["src/content/blog/incremental-quantile-estimation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-quantile-estimation.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/incremental-median-estimation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-median-estimation.mdx&astroContentModuleFlag=true")],
+["src/content/blog/incremental-quantile-estimation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-quantile-estimation.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/reading-list.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Freading-list.mdx&astroContentModuleFlag=true")]]);
 		
