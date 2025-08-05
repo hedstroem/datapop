92743c83a491afe3ae478635cfe1513035e9d128
hedstroem <john.s.hedstrom@gmail.com>
2025-08-05 15:12:18 +0200

    wrapping up sensitivity analysis page, adding blank causal modeling page

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -6,5 +6,6 @@ export default new Map([
 ["src/content/blog/sensitivity-analysis.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsensitivity-analysis.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/reading-list.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Freading-list.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/nonlinear-linear-regression.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnonlinear-linear-regression.mdx&astroContentModuleFlag=true")],
-["src/content/blog/causal-modeling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcausal-modeling.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/causal-modeling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcausal-modeling.mdx&astroContentModuleFlag=true")],
+["src/content/blog/zzz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fzzz.mdx&astroContentModuleFlag=true")]]);
 		
