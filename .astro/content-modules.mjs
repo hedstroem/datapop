94ab8fb6e149f2bad5d025b4fab1d28c85e1cfb1
hedstroem <john.s.hedstrom@gmail.com>
2025-09-30 12:42:23 +0200

    renaming some pages, changing some leadership sections

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,15 +1,16 @@
 
 export default new Map([
+["src/content/blog/embeddings.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fembeddings.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/anomaly.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fanomaly.mdx&astroContentModuleFlag=true")],
-["src/content/blog/brunton.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbrunton.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/causal-modeling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcausal-modeling.mdx&astroContentModuleFlag=true")],
-["src/content/blog/my-leadership-playbook.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmy-leadership-playbook.mdx&astroContentModuleFlag=true")],
+["src/content/blog/brunton.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbrunton.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/incremental-median-estimation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-median-estimation.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/incremental-quantile-estimation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-quantile-estimation.mdx&astroContentModuleFlag=true")],
+["src/content/blog/my-leadership-playbook.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmy-leadership-playbook.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/nonlinear-linear-regression.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnonlinear-linear-regression.mdx&astroContentModuleFlag=true")],
-["src/content/blog/particle-filters.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fparticle-filters.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/incremental-quantile-estimation-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fincremental-quantile-estimation-2.mdx&astroContentModuleFlag=true")],
+["src/content/blog/particle-filters.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fparticle-filters.mdx&astroContentModuleFlag=true")],
+["src/content/blog/stochastic-differential-equations.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fstochastic-differential-equations.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/sensitivity-analysis.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsensitivity-analysis.mdx&astroContentModuleFlag=true")],
-["src/content/blog/supply-chain-modeling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsupply-chain-modeling.mdx&astroContentModuleFlag=true")],
-["src/content/blog/stochastic-differential-equations.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fstochastic-differential-equations.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/supply-chain-modeling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsupply-chain-modeling.mdx&astroContentModuleFlag=true")]]);
 		
